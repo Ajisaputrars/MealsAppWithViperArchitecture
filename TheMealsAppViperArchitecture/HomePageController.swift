cc519164c6d745fd1537b16c22a0636e94b71e92
Aji Saputra Raka Siwi <ajisaputrars@hotmail.com>
2021-01-21 21:23:36 +0700

    init: initial commit

---
+++ TheMealsAppViperArchitecture/HomePageController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TheMealsAppViperArchitecture
+//
+//  Created by Aji Saputra Raka Siwi on 21/01/21.
+//
+
+import UIKit
+
+class HomePageController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.title = "Meals"
+  }
+}
